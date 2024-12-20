fcd252c6c640d9d598b11cd6a9b8d22359a56d29
crisleyhguimaraes <crisleyhguimaraes@gmail.com>
2024-12-20 16:48:43 -0300

    add: Visual structure -create modal and entry- exit buttons

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,6 +1,6 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
-
+import { SearchForm } from "./components/SearchForm";
 import {
     PriceHighlight,
     TransactionsContainer,
@@ -14,38 +14,31 @@ export function Transactions() {
             <Summary />
 
             <TransactionsContainer>
+                <SearchForm />
+
                 <TransactionsTable>
-                    <table>
-                        <tbody>
-                            <tr>
-                                <td width="50%">Desenvolvimento de site</td>
-                                <td>
-                                    <PriceHighlight variant="income">
-                                        R$12,000,00
-                                    </PriceHighlight>
-                                </td>
-                                <td>05/12/2024</td>
-                            </tr>
-                            <tr>
-                                <td width="50%">Despesa de hospedagem</td>
-                                <td>
-                                    <PriceHighlight variant="outcome">
-                                        - R$ 2,000,00
-                                    </PriceHighlight>
-                                </td>
-                                <td>10/12/2024</td>
-                            </tr>
-                            <tr>
-                                <td width="50%">Social Media</td>
-                                <td>
-                                    <PriceHighlight variant="income">
-                                        R$ 1.000,00
-                                    </PriceHighlight>
-                                </td>
-                                <td>15/12/2024</td>
-                            </tr>
-                        </tbody>
-                    </table>
+                    <tbody>
+                        <tr>
+                            <td width="50%">Desenvolvimento de site</td>
+                            <td>
+                                <PriceHighlight variant="income">
+                                    R$ 12.000,00
+                                </PriceHighlight>
+                            </td>
+                            <td>Venda</td>
+                            <td>13/04/2022</td>
+                        </tr>
+                        <tr>
+                            <td width="50%">Hambúrguer</td>
+                            <td>
+                                <PriceHighlight variant="outcome">
+                                    -R$ 59,00
+                                </PriceHighlight>
+                            </td>
+                            <td>Alimentação</td>
+                            <td>10/04/2022</td>
+                        </tr>
+                    </tbody>
                 </TransactionsTable>
             </TransactionsContainer>
         </div>
