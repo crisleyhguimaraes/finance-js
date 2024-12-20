c305fe00a1d2c951f430e738dd931ef7887038e3
crisleyhguimaraes <crisleyhguimaraes@gmail.com>
2024-12-20 13:36:21 -0300

    create: setup visual structure, install phosphor-react, and create Summary component

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,21 @@
+export const defaultTheme = {
+    white: "#fff",
+
+    "gray-100": "#E1E1E6",
+    "gray-300": "#C4C4CC",
+    "gray-400": "#8D8D99",
+    "gray-500": "#7C7C8A",
+    "gray-600": "#323238",
+    "gray-700": "#29292E",
+    "gray-800": "#202024",
+    "gray-900": "#121214",
+
+    "green-300": "#5CE4E4",
+    "green-500": "#34CBD9",
+    "green-700": "#2299B4",
+    "green-900": "#1B3674",
+
+    "red-300": "#F75A68",
+    "red-500": "#AB222E",
+    "red-700": "#7A1921",
+} as const;
